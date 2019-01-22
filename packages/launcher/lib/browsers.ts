507ca32a57390e6cfef35940e4d71c6acca612d3
Zach Bloomquist <zbloomq@live.com>
2019-01-22 12:51:56 -0500

    Add more supported commands for browser detection

--- packages/launcher/lib/browsers.ts
+++ packages/launcher/lib/browsers.ts
@@ -1,5 +1,5 @@
 import { log } from './log'
-import { find, map } from 'lodash'
+import { find, map, flatten, extend } from 'lodash'
 import * as cp from 'child_process'
 import { Browser, FoundBrowser, BrowserNotFoundError } from './types'
 
@@ -17,29 +17,33 @@ const browserNotFoundErr = (
 }
 
 /** list of the browsers we can detect and use by default */
-export const browsers: Browser[] = [
-  {
-    name: 'chrome',
-    displayName: 'Chrome',
-    versionRegex: /Google Chrome (\S+)/,
-    profile: true,
-    binary: 'google-chrome'
-  },
-  {
-    name: 'chromium',
-    displayName: 'Chromium',
-    versionRegex: /Chromium (\S+)/,
-    profile: true,
-    binary: 'chromium-browser'
-  },
-  {
-    name: 'canary',
-    displayName: 'Canary',
-    versionRegex: /Google Chrome Canary (\S+)/,
-    profile: true,
-    binary: 'google-chrome-canary'
-  }
-]
+export const browsers: Browser[] = flatten(
+  [
+    {
+      name: 'chrome',
+      displayName: 'Chrome',
+      versionRegex: /Google Chrome (\S+)/,
+      profile: true,
+      binary: ['google-chrome', 'google-chrome-beta', 'google-chrome-unstable']
+    },
+    {
+      name: 'chromium',
+      displayName: 'Chromium',
+      versionRegex: /Chromium (\S+)/,
+      profile: true,
+      binary: ['chromium-browser', 'chromium']
+    },
+    {
+      name: 'canary',
+      displayName: 'Canary',
+      versionRegex: /Google Chrome Canary (\S+)/,
+      profile: true,
+      binary: ['google-chrome-canary']
+    }
+  ].map(browser =>
+    browser.binary.map(binary => extend({}, browser, { binary }))
+  )
+)
 
 /** starts a browser by name and opens URL if given one */
 export function launch(
